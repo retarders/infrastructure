0b20d3382a478a4d932ab8827c04bbcbfe49f930
pcranaway <pcranaway@tuta.io>
2021-05-05 00:34:41 +0300

    attach image and make it the one that the instance boots with

--- __main__.py
+++ __main__.py
@@ -85,7 +85,10 @@ craft = openstack.compute.Instance(
         flavor_name='cc1.large',
         image_name='Debian-10.5',
         networks=[openstack.compute.InstanceNetworkArgs(name=network.name)],
-        block_devices=[openstack.compute.InstanceBlockDeviceArgs(source_type='volume', uuid=data.id, destination_type='volume')],
+        block_devices=[
+            openstack.compute.InstanceBlockDeviceArgs(source_type='image', destination_type='local', boot_index=0),
+            openstack.compute.InstanceBlockDeviceArgs(source_type='volume', uuid=data.id, destination_type='volume')
+        ],
         admin_pass=gen_password()
 )
 
